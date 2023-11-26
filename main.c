2cd61cb9ab3c26d3a0379621083dab3ee9bbbb3b
jesse <jessepoleij@hotmail.com>
2023-11-26 16:17:42 +0100

    Creation of a dynamic 2d array as gameboard. As I want to be able to insert rows and columns which wasn't viable via a static array.

--- main.c
+++ main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int** createBoard(int rows, int columns) {
+    // Allocate an array of pointers
+    int **board = malloc(rows * sizeof(int*));
+    if (board == NULL) {
+        return NULL;
+    }
+
+    // Allocate each row
+    for (int i = 0; i < rows; i++) {
+        board[i] = malloc(columns * sizeof(int));
+        if (board[i] == NULL) {
+            // Free previously allocated rows in case of failure
+            for (int j = 0; j < i; j++) {
+                free(board[j]);
+            }
+            free(board);
+            return NULL;
+        }
+    }
+
+    return board;
+}
+
+void freeBoard(int **board, int rows) {
+    if (board != NULL) {
+        // Free each row
+        for (int i = 0; i < rows; i++) {
+            free(board[i]);
+        }
+        // Free the array of pointers
+        free(board);
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    bool isPlayerOnesTurn = true;
+
+    int rows = 5;
+    int columns = 5;
+
+    int **board = createBoard(rows, columns);
+
+    int value = board[1][1];
+
+    printf("Test value: %d", value);
+
+    freeBoard(board, rows);
     return 0;
 }
