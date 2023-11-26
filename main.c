0f990ba27907d5c99b3fde0f28148eabcbb9df30
jesse <jessepoleij@hotmail.com>
2023-11-26 17:30:08 +0100

    Seperate board from main.c. In the future pieces should probably be in a pieces specific file.

--- main.c
+++ main.c
@@ -1,39 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int** createBoard(int rows, int columns) {
-    // Allocate an array of pointers
-    int **board = malloc(rows * sizeof(int*));
-    if (board == NULL) {
-        return NULL;
-    }
-
-    // Allocate each row
-    for (int i = 0; i < rows; i++) {
-        board[i] = malloc(columns * sizeof(int));
-        if (board[i] == NULL) {
-            // Free previously allocated rows in case of failure
-            for (int j = 0; j < i; j++) {
-                free(board[j]);
-            }
-            free(board);
-            return NULL;
-        }
-    }
-
-    return board;
-}
-
-void freeBoard(int **board, int rows) {
-    if (board != NULL) {
-        // Free each row
-        for (int i = 0; i < rows; i++) {
-            free(board[i]);
-        }
-        // Free the array of pointers
-        free(board);
-    }
-}
+#include "src/board.h"
 
 int main() {
     bool isPlayerOnesTurn = true;
@@ -41,12 +7,10 @@ int main() {
     int rows = 5;
     int columns = 5;
 
-    int **board = createBoard(rows, columns);
+    Pieces ***board = createBoard(rows, columns);
 
-    int value = board[1][1];
+    Pieces *value_pointer = board[1][1];
 
-    printf("Test value: %d", value);
-
-    freeBoard(board, rows);
+    freeBoard(board, rows, columns);
     return 0;
 }
