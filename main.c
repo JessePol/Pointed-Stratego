f3744d4902e4db0f4584fb7b142434f98e987b84
jesse <jessepoleij@hotmail.com>
2023-11-29 14:19:32 +0100

    Beginning of turn.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #include "src/board.h"
 
@@ -17,13 +18,61 @@ Pieces*** initializeBoard(int rows, int columns){
     Pieces* newPiece = createPiece(true, 10);
     board[1][1] = newPiece;
 
-
     return board;
 }
 
 
+
+bool turn(Pieces*** board, bool isPlayerOne){
+    int xCoordinate;
+    int yCoordinate;
+
+    printf("Enter the X coordinate: ");
+    scanf("%d", &xCoordinate);
+
+    printf("Enter the Y coordinate: ");
+    scanf("%d", &yCoordinate);
+
+    Pieces* selected_piece = board[xCoordinate][yCoordinate];
+
+    if (selected_piece == NULL){
+        printf("You selected an empty field.");
+        return false;
+    }
+
+    if (selected_piece->isPlayerOne != isPlayerOne){
+        printf("You selected an enemy piece.");
+        return false;
+    }
+
+    char direction;
+
+    printf("Enter a direction the first letter of (Right/Left/Up/Down): ");
+    scanf(" %c", &direction);
+
+    direction = toupper(direction); // Convert to uppercase for consistency
+
+    switch (direction) {
+        case 'R':
+            printf("You entered Right.\n");
+            break;
+        case 'L':
+            printf("You entered Left.\n");
+            break;
+        case 'U':
+            printf("You entered Up.\n");
+            break;
+        case 'D':
+            printf("You entered Down.\n");
+            break;
+        default:
+            printf("Invalid direction entered.\n");
+    }
+
+    return true;
+}
+
 int main() {
-    bool isPlayerOnesTurn = true;
 
     int rows = 5;
     int columns = 5;
@@ -31,6 +80,8 @@ int main() {
     Pieces ***board = initializeBoard(rows, columns);
 
     printBoard(board, rows, columns);
+
+    turn(board, true);
 
     freeBoard(board, rows, columns);
     return 0;
--- src/board.h
+++ src/board.h
@@ -7,6 +7,13 @@ typedef struct {
     bool isPlayerOne;
 } Pieces;
 
+typedef struct {
+    int rows;
+    int columns;
+    Pieces*** gameBoard;
+} Board;
+
+
 Pieces*** createBoard(int rows, int columns);
 void freeBoard(Pieces ***board, int rows, int columns);
 void printBoard(Pieces ***board, int rows, int columns);
